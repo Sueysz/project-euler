0e51c64027a2cb12149e5b76034834bbe448ad22
sueys <sueys@live.fr>
2022-08-19 00:47:15 +0200

    résolution en c du 01 project euler

--- main.c
+++ main.c
@@ -3,12 +3,19 @@
 
 void main()
 {
- //je veux une variable qui s'appel R dedans il y à 3+3
- int i = 0;
- //je veux une restriction qui vas jusqu'à 1000 en utilisant R
- while (i < 1000) {
-    printf(i);
-    i = i + 1;
- }
- gets()
+//je veux une variable qui s'appel R dedans il y à 3+3
+//création de e la somme de tous les modulos
+    int i = 0;
+    int e = 0;
+//je veux une restriction qui vas jusqu'à 1000 en utilisant R
+    while (i < 1000)
+    {
+        i = i + 1;
+        if (i % 3 == 0 || i % 5 == 0) {
+            e=e+i;
+
+        }
+    }
+        printf("%d\n", e);
+
 }
